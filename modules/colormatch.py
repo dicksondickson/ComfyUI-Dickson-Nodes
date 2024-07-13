240283021ea5ef312fc6edd89469e8fdcc2c85ef
Dickson <dickson@dicksonchow.com>
2024-07-13 12:25:56 -0700

    a

--- modules/colormatch.py
+++ modules/colormatch.py
@@ -79,10 +79,8 @@ def wavelet_color_match(target: Tensor, source: Tensor):
     print("[DICKSON-NODES] wavelet_color_match")
     
     
-    source = tensor2pil(source)
-    source = source.resize(target.size, resample=Image.Resampling.LANCZOS)
-    
-
+    t_source = tensor2pil(source)
+    source = t_source.resize(target.size, resample=Image.Resampling.LANCZOS)
     
 
     # Convert images to tensors
